8e79440155706b490f9fd1397a2893360890d53f
MahamZehra <31242906+MahamZehra@users.noreply.github.com>
2019-02-12 15:38:43 +0500

    Add files via upload

---
+++ Lab2b/top.v
@@ -0,0 +1,28 @@
+module top
+(
+ input clk, reset, rx,
+ output [3:0] q,
+ output tx 
+);
+
+wire [7:0] counter_data;
+assign counter_data = {4'b0000,q};
+  
+  ripple_carry_counter rcc0
+  (
+    .clk(clk),
+    .reset(reset),
+    .q(q)
+  );
+  
+  uart_top uart0
+  (
+    .clk(clk),
+    .reset(reset),
+    .data_to_tx(counter_data),
+    .rx(rx),
+    .tx(tx)
+  );
+  
+ endmodule
+ 
